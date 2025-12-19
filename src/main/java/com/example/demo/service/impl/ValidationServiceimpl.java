c6f2dfdc368e89af4496139dde32d39101200eb7
bala-amypo <bala@amypo.in>
2025-12-19 04:40:30 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/ValidationServiceimpl.java
@@ -16,6 +16,6 @@ public class ValidationServiceImpl implements ValidationService{
           }
           @Override
           public ValidationEntity findData(Long id){
-               return dent.findById(id);
+               return dent.findById(id).orElse(other:null);
           }
 }
