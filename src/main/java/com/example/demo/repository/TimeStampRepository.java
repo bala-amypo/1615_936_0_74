323abd5d82af32ea69f526594b93c16fd0bb4fbf
bala-amypo <bala@amypo.in>
2025-12-19 08:10:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/TimeStampRepository.java
+++ src/main/java/com/example/demo/repository/TimeStampRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
-
-
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.ValidationEntity;
 
 @Repository
-public interface TimeStampRepository{
-    
+public interface TimeStampRepository extends JpaRepostiory<TimeStampEntity,Long>{
+
 }
