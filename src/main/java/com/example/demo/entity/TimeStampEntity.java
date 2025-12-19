a45d794437ab5de0661b185d8881348d6b13a6f3
bala-amypo <bala@amypo.in>
2025-12-19 07:58:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/TimeStampEntity.java
+++ src/main/java/com/example/demo/entity/TimeStampEntity.java
@@ -12,6 +12,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 
 public class TimeStampEntity{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
      private Long id;
      private String name;
      private String email;
